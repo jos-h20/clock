91688c6e8a1e44784df3c3589383d7701f0afa94
jos-h20 <josh_overly@yahoo.com>
2016-02-15 21:21:34 -0800

    make betweenhands spec work

--- src/Clock.php
+++ src/Clock.php
@@ -11,12 +11,12 @@ class Clock
         $minute_degrees = $input_minute/60 * 360;
         return $minute_degrees;
     }
-    function betweenHands($input_minute, $input_hour)
+    function betweenHands($input_hour, $input_minute)
     {
         $chank = $input_hour * 60;
         $chunk = $input_minute * 11;
         $chunker = $chank - $chunk;
-        $clock_angle = .5 * $chunker;
+        $clock_angle = abs($chunker/2);
 
         if ($clock_angle > 180) {
             $clock_angle = 360 - $clock_angle;
